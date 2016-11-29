c217104d89baa961b0a5615f105054f1bbb3a8e9
janglex <jan@gunther.com>
2016-11-29 15:28:29 -0500

    Add files via upload

--- map.js
+++ map.js
@@ -2,22 +2,23 @@
 var RochesterCustomerList = [];
 var WebsterCustomerList = [];
 
+//initiate two lists of sample customer data
 CustomersfromLines(RochesterCustomers, RochesterCustomerList);
 CustomersfromLines(WebsterCustomers, WebsterCustomerList);
 
+//arbitrarily choose Rochester customers to display
 var customersToDisplay = RochesterCustomerList;
 
-
+//Find the latitude of a customer object or assume a paired list of lat, lng and return the lat.
 function findLatitude (object) {
   if (object instanceof customer) {
-    //lert ("Lat is "+ object.latitude);
     return object.latitude;
   } else {
-    //alert ("Lat is "+ object.latitude);
     return object[0];
   }
 }
 
+//Find the latitude of a customer object or assume a paired list of lat, lng and return the lng.
 function findLongitude (object) {
   if (object instanceof customer) {
     return object.longitude;
@@ -26,7 +27,7 @@ function findLongitude (object) {
   }
 }
 
-// takes an array of objects, uses methods to find lat and lng
+//Finds a center point of an array of points or an array of customers
 function centerPoint(points) {
   var count = 0.0;
   var sumlat = 0;
@@ -42,68 +43,81 @@ function centerPoint(points) {
   return ([sumlat/count, sumlng/count]);
 }
 
-//If you don't give a marker a map to draw on, it just hangs around until you display it.
+//Make a marker for each customer on the customerList but don't display it yet
 function makeMarkers(customerList) {
-  var markerList = [];
+  //var markerList = [];
   for (var i = 0; i < customerList.length; i++) {
-    var lat = findLatitude(customerList[i]);
-    var lng = findLongitude(customerList[i]);
-    var point = {lat: lat, lng: lng};
-    var marker = new google.maps.Marker({
-          position: point,
-          title: customerList[i].address
-      });
-    markerList.push(marker);
+    if (customerList.marker == null) {
+      var lat = findLatitude(customerList[i]);
+      var lng = findLongitude(customerList[i]);
+      var point = {lat: lat, lng: lng};
+      var marker = new google.maps.Marker({
+            position: point,
+            title: customerList[i].address
+        });
+      //markerList.push(marker);
+      customerList[i].marker = marker;
+    }
   }
-  return markerList;
+  //return markerList;
 }
 
-var RochesterCustomerMarkers = makeMarkers(RochesterCustomerList);
-var WebsterCustomerMarkers = makeMarkers(WebsterCustomerList);
-
-
-//Display the list of markers on a map
-function displayMarkersOnMap(markerlist, map) {
-  for (var i = 0; i < markerlist.length; i++) {
-    markerlist[i].setMap(map);
+//Display the list of customer markers on a map - iterate through the customers and setMap for each marker
+//use map = null to remove markers
+function displayCustomerMarkersOnMap(customerlist, map) {
+  setMapOnAll(customerlist,map);
+  /*
+  for (var i = 0; i < customerlist.length; i++) {
+    var marker = customerlist[i].marker;
+    marker.setMap(map);
   }
+  */
 }
 
-//Remove the list of markers from a map
-function removeMarkersFromMap(markerlist, map) {
- for (var i = 0; i < markerlist.length; i++) {
-    markerlist[i].setMap(null);
-  }
-}
+// Sets the map on all markers in the array.
+function setMapOnAll(List, map) {
+  for (var i = 0; i < List.length; i++) {
+    var marker;
+    if (List[i] instanceof customer) {
+      marker = List[i].marker;
+    } else {
+        marker = List[i];
+    }
+    marker.setMap(map);
+  }}
 
-function swapCustomerArea () {
+
+
+function swapCustomerArea1 () {
   if (customersToDisplay === RochesterCustomerList) {
     customersToDisplay = WebsterCustomerList;
   } else {
     customersToDisplay = RochesterCustomerList;
   }
+  addMarkstoMap(customersToDisplay);
   initMap();
 }
 
-/*
+
 function swapCustomerArea () {
   if (customersToDisplay === RochesterCustomerList) {
     customersToDisplay = WebsterCustomerList;
-    removeMarkersFromMap(RochesterCustomerMarkers);
-    displayMarkersOnMap(WebsterCustomerMarkers);
+    displayCustomerMarkersOnMap(RochesterCustomerList,null);
+    displayCustomerMarkersOnMap(WebsterCustomerList,map);
   } else {
     customersToDisplay = RochesterCustomerList;
-    removeMarkersFromMap(WebsterCustomerMarkers);
-    displayMarkersOnMap(RochesterCustomerMarkers);
+    displayCustomerMarkersOnMap(WebsterCustomerList,null);
+    displayCustomerMarkersOnMap(RochesterCustomerList,map);
   }
   //initMap();
 }
-*/
 
+//Keep track of map, so we don't have to keep creating new ones.
+var map;
 function initMap() {
   var customerList = customersToDisplay;
   var centerPt = centerPoint(customerList);
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: {lat: centerPt[0], lng: centerPt[1]}
   });
@@ -119,10 +133,11 @@ function initMap() {
     });
   }
   */
-  var markers = makeMarkers(customersToDisplay);
-  displayMarkersOnMap(markers, map);
+  makeMarkers(customersToDisplay);
+  displayCustomerMarkersOnMap(customersToDisplay, map);
 }
 
 function addMarkstoMap() {
-  displayMarkersOnMap(customersToDisplay);
+  makeMarkers(customersToDisplay);
+  displayCustomerMarkersOnMap(customersToDisplay, map);
 }
